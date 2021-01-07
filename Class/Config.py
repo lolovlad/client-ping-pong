188b0fe594c866a39ab0c3102695f992ef141b57
Vlad <lll-ooo-2003@mail.ru>
2021-01-07 12:08:38 +0400

    - создана система конфигураций - переделана система создания игрового поля

--- Class/Config.py
+++ Class/Config.py
@@ -12,6 +12,10 @@ class Config:
         with open(self.__path, "r") as read_file:
             self.__config = json.load(read_file)
 
+    def save(self):
+        with open(self.__path, 'w') as outfile:
+            json.dump(self.__config, outfile)
+
     def get_color(self, name):
         return tuple(self.__config["Color"][name])
 
@@ -20,6 +24,16 @@ class Config:
 
     def get_window(self, name):
         return int(self.__config["Window"][name])
+
+    def set_color(self, name, info):
+        self.__config["Color"][name] = list(info)
+
+    def set_position(self, name, info):
+        self.__config["Position"][name] = list(info)
+
+    def set_window(self, name, info):
+        self.__config["Window"][name] = info
+
 
 
 
